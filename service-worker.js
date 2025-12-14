55cf55fc2273dee22a6fc45a00d4c63c9886c084
Cony <shimz777@gmail.com>
2025-12-14 13:35:34 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -15,14 +15,6 @@ const messaging = firebase.messaging();
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: './images/wordrobe-icon.png',
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
-
   if (navigator.setAppBadge) {
       navigator.setAppBadge(1).catch((error) => {
           console.error('Badge error:', error);
